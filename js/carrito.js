4972cd87acac62df18b63ee3801a2b66ab1067f3
Ivana Vega <ivanavega86@gmail.com>
2025-12-01 20:31:41 -0300

    Actualizacion

--- js/carrito.js
+++ js/carrito.js
@@ -1,74 +1,42 @@
 
-//  productos disponibles 
-const producto1 = "BODY MICROFIBRA CON PUNTILLA";
-const producto2 = "CROP ALGODON DISORDER";
-const producto3 = "TOP ALGODON SHELL";
-const producto4 = "TOP ENCAJE";
+//productos disponibles
+const productos = [
+  { id: 1, nombre: "BODY MICROFIBRA CON PUNTILLA", precio: 7500, imagen:"./assets/body.jpg" },
+  { id: 2, nombre: "CROP ALGODON DISORDER", precio: 8400, imagen:"./assets/crop.jpg" },
+  { id: 3, nombre: "TOP ALGODON SHELL", precio: 8000, imagen:"./assets/cropshell.jpg" },
+  { id: 4, nombre: "TOP ENCAJE", precio: 8000, imagen:"./assets/encaje.jpg" },
+];
 
-const precio1 = 7500;
-const precio2 = 8400;
-const precio3 = 8000;
-const precio4 = 8000;
+let carrito = [];
+let contenedor = document.querySelector("#catalogo");
+contenedor.innerHTML="";
+productos.forEach((prod) => {
+  let div = document.createElement ("div");
+  div.innerHTML = `
+  <div class= "producto">
+  <img src="${prod.imagen}" alt="${prod.nombre}" width="250">
+  <span>${prod.id}</span>
+  <h3>${prod.nombre}</h3>
+  <p>${prod.precio}</p>
+  <button data-id=${prod.id} class='btnAgregar'>Agregar al Carrito</button>
+  </div>
+  `;
+  contenedor.appendChild(div);
+});
+let btnAgregar = document.querySelectorAll(".btnAgregar");
+btnAgregar.forEach((btn) =>{
+  btn.addEventListener("click", (e)=>{
+  let idSelected= e.target.dataset.id;
+ 
+  let productoSelected = productos.find((prod) => prod.id == idSelected);
+  carrito.push(productoSelected);
+  console.log(carrito);
+  
+  });
+});
 
-//  variables para el carrito
-let total = 0;
-let cantidadProductos = 0;
+let h1 = document.getElementById("h1")
+h1.innerHTML ="Carrito de Compras" 
 
-//  Mostrar productos 
-function mostrarProductos() {
-  console.log(" PRODUCTOS DISPONIBLES:");
-  console.log("1 - " + producto1 + " ($" + precio1 + ")");
-  console.log("2 - " + producto2 + " ($" + precio2 + ")");
-  console.log("3 - " + producto3 + " ($" + precio3 + ")");
-  console.log("4 - " + producto4 + " ($" + precio4 + ")");
-}
-
-// Agregar productos al carrito 
-function agregarAlCarrito() {
-  let continuar = true;
-
-  while (continuar) {
-    let opcion = prompt(
-      `Ingrese el número del producto que desea comprar:\n1. ${producto1}\n2. ${producto2}\n3. ${producto3}\n4. ${producto4}`
-    );
-
-    if (opcion == "1") {
-      total = total + precio1;
-      cantidadProductos++;
-      alert("Agregaste: " + producto1);
-    } else if (opcion == "2") {
-      total = total + precio2;
-      cantidadProductos++;
-      alert("Agregaste: " + producto2);
-    } else if (opcion == "3") {
-      total = total + precio3;
-      cantidadProductos++;
-      alert("Agregaste: " + producto3);
-    } else if (opcion == "4") {
-      total = total + precio4;
-      cantidadProductos++;
-      alert("Agregaste: " + producto4);
-    } else {
-      alert("Opción incorrecta. Intente nuevamente.");
-    }
-
-    continuar = confirm("¿Querés agregar otro producto?");
-  }
-}
-
-//  Mostrar el total 
-function mostrarTotal() {
-  alert(
-    "Agregaste " + cantidadProductos + " productos al carrito.\n" +
-    "El total a pagar es: $" + total
-  );
-
-  console.log("Productos comprados: " + cantidadProductos);
-  console.log("Total: $" + total);
-}
-
-alert("Bienvenida a la tienda");
-mostrarProductos();
-agregarAlCarrito();
-mostrarTotal();
-alert("¡Gracias por tu compra!");
+let p = document.getElementById("p")
+p.innerHTML = "Bienvenidos a nuestra tienda, todos nuestros productos son importados."
