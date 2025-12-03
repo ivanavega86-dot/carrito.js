b18c7096134775121e22b3b754eae55b69399bf9
Ivana Vega <ivanavega86@gmail.com>
2025-12-03 16:32:05 -0300

    modificaciones

--- js/carrito.js
+++ js/carrito.js
@@ -5,38 +5,73 @@ const productos = [
   { id: 2, nombre: "CROP ALGODON DISORDER", precio: 8400, imagen:"./assets/crop.jpg" },
   { id: 3, nombre: "TOP ALGODON SHELL", precio: 8000, imagen:"./assets/cropshell.jpg" },
   { id: 4, nombre: "TOP ENCAJE", precio: 8000, imagen:"./assets/encaje.jpg" },
+{id:5, nombre:"TOP MICROFIBRA ESCOTE CON HOOK",precio:6900, imagen:"./assets/escotehook.jpg"},
+{id:6, nombre:"SHORT POLLERA", precio:15000, imagen:"./assets/shortpollera.jpg"},
+{id:7, nombre:"TOP DE TIRAS", precio:6900, imagen:"./assets/tiras.jpg"},
+{id:8, nombre:"TOP MICROPUNTILLAS", precio:7100, imagen:"../assets/topmicropuntillas.jpg"},
 ];
 
 let carrito = [];
+
+carrito = JSON.parse(localStorage.getItem("carrito")) || [];
+
 let contenedor = document.querySelector("#catalogo");
-contenedor.innerHTML="";
-productos.forEach((prod) => {
-  let div = document.createElement ("div");
-  div.innerHTML = `
-  <div class= "producto">
-  <img src="${prod.imagen}" alt="${prod.nombre}" width="250">
-  <span>${prod.id}</span>
-  <h3>${prod.nombre}</h3>
-  <p>${prod.precio}</p>
-  <button data-id=${prod.id} class='btnAgregar'>Agregar al Carrito</button>
-  </div>
-  `;
-  contenedor.appendChild(div);
-});
+
+function renderProductos(lista) {
+  contenedor.innerHTML = "";
+
+  lista.forEach((prod) => {
+    let div = document.createElement("div");
+    div.innerHTML = `
+      <div class="producto">
+        <img src="${prod.imagen}" alt="${prod.nombre}" width="250">
+        <h6>${prod.nombre}</h6>
+        <p>$${prod.precio}</p>
+        <button data-id=${prod.id} class='btnAgregar'>Agregar al Carrito</button>
+      </div>
+    `;
+    contenedor.appendChild(div);
+  });
+
+  actualizarBotones();
+}
+function actualizarBotones() {
+  
 let btnAgregar = document.querySelectorAll(".btnAgregar");
 btnAgregar.forEach((btn) =>{
   btn.addEventListener("click", (e)=>{
-  let idSelected= e.target.dataset.id;
- 
-  let productoSelected = productos.find((prod) => prod.id == idSelected);
-  carrito.push(productoSelected);
-  console.log(carrito);
-  
+    let idSelected= e.target.dataset.id;
+    
+    let productoSelected = productos.find((prod) => prod.id == idSelected);
+    carrito.push(productoSelected);
+
+    localStorage.setItem("carrito", JSON.stringify(carrito));
+    console.log("Carrito actualizado", carrito);
+    
   });
 });
+}
+renderProductos(productos);
 
-let h1 = document.getElementById("h1")
-h1.innerHTML ="Carrito de Compras" 
+//storage
+localStorage.setItem("productos", JSON.stringify(productos));
+
+const buscador = document.getElementById("buscador");
+
+buscador.addEventListener("input", () => {
+  const texto = buscador.value.toLowerCase();
+  const productosLS = JSON.parse(localStorage.getItem("productos")) || [];
+  const filtrados = productosLS.filter(prod =>
+    prod.nombre.toLowerCase().includes(texto)
+  );
+
+ 
+  renderProductos(filtrados);
+});
+
+let h3 = document.getElementById("h3")
+h3.innerHTML ="Carrito de compras" 
 
 let p = document.getElementById("p")
 p.innerHTML = "Bienvenidos a nuestra tienda, todos nuestros productos son importados."
+
